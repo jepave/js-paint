c8277309f2c280274e68b3bdf504b7daafc14402
Jesspv <41031248+jepave@users.noreply.github.com>
2022-04-23 21:29:22 -0700

    Add files via upload

---
+++ shsapes.js
@@ -0,0 +1,21 @@
+html
+  <!-- Shapes -->
+    <div class="tools">
+      <label for="shapes">Shapes</label>
+      <div class="select-dropdown">
+      <select>
+        <option <a id="line" <i class="fa-light fa-horizontal-rule"title="line"></i></a></option>
+        <option <a id="circle" <i class="fa-light fa-circle"title="circle"></i></a></option>
+        <option <a id="square" <i class="fa-light fa-square"title="square"></i></a></option>
+        <option <a id="polygon" <i class="fa-light fa-hexagon"title="polygon"></i></a></option>
+      </select>
+      </div>
+    </div>
+
+
+
+     document.getElementById("line").className = "";
+    document.getElementById("rectangle").className = "";
+    document.getElementById("circle").className = "";
+    document.getElementById("polygon").className = "";
+
